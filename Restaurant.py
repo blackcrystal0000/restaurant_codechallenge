2bab1a75bed63d69bb3450ee4651cbaa256c7e6b
Crystal <muthonigachanja2000@gmail.com>
2023-08-28 15:57:20 -0700

    Use the getter method

--- Restaurant.py
+++ Restaurant.py
@@ -10,3 +10,15 @@ class Customer:
         self.reviews = []
         # Add the customer to the list of all customers
         Customer.all_customers.append(self)
+
+        # Define a getter method to return the given name property
+    def given_name(self):
+        return self.given_name
+    
+    # Define a getter method to return the family name property
+    def family_name(self):
+        return self.family_name
+    
+    # Define a method to return the full name of the customer in Western style
+    def full_name(self):
+        return f'{self.family_name} {self.given_name}'
