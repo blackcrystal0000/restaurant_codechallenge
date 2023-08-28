4a37836e9a105efed37b254b7610a972dc58cc6d
Crystal <muthonigachanja2000@gmail.com>
2023-08-28 15:56:23 -0700

    Set given name and family name

--- Restaurant.py
+++ Restaurant.py
@@ -2,3 +2,11 @@
 class Customer:
     # Initialize a class variable to store all customers
     all_customers = []
+
+     # Define the constructor method to set the given name and family name properties
+    def __init__(self, given_name, family_name):
+        self.given_name = given_name
+        self.family_name = family_name
+        self.reviews = []
+        # Add the customer to the list of all customers
+        Customer.all_customers.append(self)
